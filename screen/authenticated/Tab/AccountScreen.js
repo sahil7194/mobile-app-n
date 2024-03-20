97a7c9bbc568c95b4cac0224d6b1dafef429882c
Sahil Tamboli <tambolisahil36@gmail.com>
2024-03-21 02:08:25 +0530

    logout done

--- screen/authenticated/Tab/AccountScreen.js
+++ screen/authenticated/Tab/AccountScreen.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Text, View ,SafeAreaView ,StatusBar , Button } from 'react-native'
+import { AuthContext } from '../../../Context/AuthContext';
+import { removeData } from '../../../services/LocalStorageService'
 
 export const AccountScreen = ({ navigation }) => {
+
+  const {auth , setAuth} = useContext(AuthContext);
+
+  const logout = () => {
+    // remove token and navigate to login screen
+    console.log('Logging out');
+    removeData("auth_token");
+    setAuth(false);
+  }
+
   return (
     <SafeAreaView style={{ marginTop: StatusBar.currentHeight || 0 }}>
       <View>
@@ -15,6 +27,10 @@ export const AccountScreen = ({ navigation }) => {
       <Button
         title="Go to EditProfile"
         onPress={() => navigation.navigate('EditProfile')}
+      />
+      <Button
+        title="Log Out"
+        onPress={() => logout()}
       />
       </View>
     </SafeAreaView>
