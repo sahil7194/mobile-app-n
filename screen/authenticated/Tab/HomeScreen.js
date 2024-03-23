70f8fa9bbbc4fafadb13a04392edb04b01109573
Sahil Tamboli <tambolisahil36@gmail.com>
2024-03-23 17:40:46 +0530

    api services update

--- screen/authenticated/Tab/HomeScreen.js
+++ screen/authenticated/Tab/HomeScreen.js
@@ -1,74 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, View ,SafeAreaView ,StatusBar ,Button, FlatList  } from 'react-native'
-import { retrieveData } from '../../../services/LocalStorageService';
 import { RenderCategory } from '../../../components/authenticated/RenderCategory';
+import { get } from '../../../services/ApiServices';
 
 export const HomeScreen = ({ navigation }) => {
 
-  const categoryData = [
-    {
-        "id": 1,
-        "name": "Kelley Schmeler",
-        "image": "https://source.unsplash.com/random/200x200?sig=12"
-    },
-    {
-        "id": 2,
-        "name": "Mrs. Arlene Wolf",
-        "image": "https://source.unsplash.com/random/200x200?sig=2"
-    },
-    {
-        "id": 3,
-        "name": "Mr. Zackery Kautzer",
-        "image": "https://source.unsplash.com/random/200x200?sig=4"
-    },
-    {
-        "id": 4,
-        "name": "Katlyn Trantow",
-        "image": "https://source.unsplash.com/random/200x200?sig=11"
-    },
-    {
-        "id": 5,
-        "name": "Baby Stark",
-        "image": "https://source.unsplash.com/random/200x200?sig=6"
-    },
-    {
-        "id": 6,
-        "name": "Bethel Doyle",
-        "image": "https://source.unsplash.com/random/200x200?sig=17"
-    },
-    {
-        "id": 7,
-        "name": "Laron Blick",
-        "image": "https://source.unsplash.com/random/200x200?sig=3"
-    },
-    {
-        "id": 8,
-        "name": "Jaime Dooley",
-        "image": "https://source.unsplash.com/random/200x200?sig=12"
-    },
-    {
-        "id": 9,
-        "name": "Angus Wyman",
-        "image": "https://source.unsplash.com/random/200x200?sig=2"
-    },
-    {
-        "id": 10,
-        "name": "Wanda Rodriguez",
-        "image": "https://source.unsplash.com/random/200x200?sig=12"
-    }
-];
+  const [categoryData , setCategoryData] = useState([]);
+
+  const getCategory = () => {
+      get('categories').then((response) => {
+        setCategoryData(response.data);
+        console.log('data get ');
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
   
+  categoryData.length === 0 ? getCategory() : null;
 
   return (
     <SafeAreaView style={{ marginTop: StatusBar.currentHeight || 0 }}>
-      {/* <View>
-      <Text className="border border-red-500">
-        Home Screen
-      </Text>
 
-     
-
-      </View> */}
       <View className="border border-red-500">
         <Text>
           search compoent 
--- services/ApiServices.js
+++ services/ApiServices.js
@@ -3,9 +3,11 @@ import axios from "axios";
 import envirnoments from '../environments/environments';
 import {retrieveData } from './LocalStorageService';
 
-const apiUrl = envirnoments.apiUrl;
+// TODO: need to update logic
+// export const apiUrl = envirnoments.apiUrl;
+export const apiUrl = 'http://192.168.1.64/api/';
 
-const authToken = retrieveData('authToken').
+export const authToken = retrieveData('authToken').
 then((token) => {
      return token
     }).catch((err) =>{
@@ -20,33 +22,50 @@ const client = axios.create({
   }
 });
 
-export const get = (path,queryParams) =>{
-    client.get(path, {params : queryParams})
-      .then((response)=>{
-        return response;
-      }).catch((err)=>console.log(err))
-}
+export const get = (path, queryParams) => {
+    return new Promise((resolve, reject) => {
+        client.get(path, { params: queryParams })
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+};
 
-export const post = (path, postData , queryParams) => {
-    client.post(path, postData, { params : queryParams }).then((response) =>{
-        return response;
-    }).catch((error) =>{
-        console.log(error);
-    })
-}
+export const post = (path, postData, queryParams) => {
+    return new Promise((resolve, reject) => {
+        client.post(path, postData, { params: queryParams })
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
 
-export const  put = (path, postData, queryParams) => {
-    client.post(path, postData, { params : queryParams }).then((response) =>{
-        return response;
-    }).catch((error) =>{
-        console.log(error);
-    })
-}
+export const put = (path, postData, queryParams) => {
+    return new Promise((resolve, reject) => {
+        client.put(path, postData, { params: queryParams })
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
 
 export const deleteM = (path, queryParams) => {
-    client.post(path,{ params : queryParams }).then((response) =>{
-        return response;
-    }).catch((error) =>{
-        console.log(error);
-    })
-}
+    return new Promise((resolve, reject) => {
+        client.delete(path, { params: queryParams })
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
